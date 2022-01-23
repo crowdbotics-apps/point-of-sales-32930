8ede8afc8fd167fa53c545bfce3d0eea4c48dc52
asedekntl59969@gmail.com <asedekntl59969@gmail.com>
2022-01-23 18:32:26 +0700

    Update screen

--- screens/Produk(30:544)1271304_30_544/index.js
+++ screens/Produk(30:544)1271304_30_544/index.js
@@ -14,10 +14,6 @@ import {
 import Icon from "react-native-vector-icons/FontAwesome"
 import { CheckBox } from "react-native-elements"
 import { connect } from "react-redux"
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp
-} from "react-native-responsive-screen"
 import { getNavigationScreen } from "@screens"
 export class Blank extends React.Component {
   constructor(props) {
@@ -171,34 +167,34 @@ export class Blank extends React.Component {
 
 const styles = StyleSheet.create({
   ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
-  View_2: { height: hp("112%") },
+  View_2: { height: ("112%") },
   View_30_545: {
-    width: wp("100%"),
-    height: hp("114%"),
-    top: hp("-1%"),
+    width: ("100%"),
+    height: ("114%"),
+    top: ("-1%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("0%"),
+    left: ("0%"),
     backgroundColor: "rgba(24, 52, 64, 1)"
   },
   View_30_546: {
-    width: wp("100%"),
-    height: hp("113%"),
-    top: hp("10%"),
+    width: ("100%"),
+    height: ("113%"),
+    top: ("10%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("475%"),
+    left: ("475%"),
     backgroundColor: "rgba(24, 52, 64, 1)"
   },
   View_30_547: {
-    width: wp("35%"),
-    top: hp("14%"),
+    width: ("35%"),
+    top: ("14%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     justifyContent: "flex-start"
   },
   Text_30_547: {
@@ -211,12 +207,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_548: {
-    width: wp("51%"),
-    top: hp("26%"),
+    width: ("51%"),
+    top: ("26%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     justifyContent: "flex-start"
   },
   Text_30_548: {
@@ -229,12 +225,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_549: {
-    width: wp("51%"),
-    top: hp("37%"),
+    width: ("51%"),
+    top: ("37%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     justifyContent: "flex-start"
   },
   Text_30_549: {
@@ -247,12 +243,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_550: {
-    width: wp("51%"),
-    top: hp("50%"),
+    width: ("51%"),
+    top: ("50%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     justifyContent: "flex-start"
   },
   Text_30_550: {
@@ -265,12 +261,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_551: {
-    width: wp("51%"),
-    top: hp("63%"),
+    width: ("51%"),
+    top: ("63%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("481%"),
+    left: ("481%"),
     justifyContent: "flex-start"
   },
   Text_30_551: {
@@ -283,72 +279,72 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_552: {
-    width: wp("85%"),
-    height: hp("6%"),
-    top: hp("18%"),
+    width: ("85%"),
+    height: ("6%"),
+    top: ("18%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     backgroundColor: "rgba(209, 220, 190, 1)"
   },
   View_30_553: {
-    width: wp("85%"),
-    height: hp("6%"),
-    top: hp("31%"),
+    width: ("85%"),
+    height: ("6%"),
+    top: ("31%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     backgroundColor: "rgba(209, 220, 190, 1)"
   },
   View_30_554: {
-    width: wp("85%"),
-    height: hp("6%"),
-    top: hp("42%"),
+    width: ("85%"),
+    height: ("6%"),
+    top: ("42%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     backgroundColor: "rgba(209, 220, 190, 1)"
   },
   View_30_555: {
-    width: wp("85%"),
-    height: hp("6%"),
-    top: hp("55%"),
+    width: ("85%"),
+    height: ("6%"),
+    top: ("55%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("479%"),
+    left: ("479%"),
     backgroundColor: "rgba(209, 220, 190, 1)"
   },
   View_30_556: {
-    width: wp("85%"),
-    height: hp("6%"),
-    top: hp("105%"),
+    width: ("85%"),
+    height: ("6%"),
+    top: ("105%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("481%"),
+    left: ("481%"),
     backgroundColor: "rgba(252, 255, 246, 1)"
   },
   View_30_557: {
-    width: wp("85%"),
-    height: hp("35%"),
-    top: hp("67%"),
+    width: ("85%"),
+    height: ("35%"),
+    top: ("67%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("481%"),
+    left: ("481%"),
     backgroundColor: "rgba(209, 220, 190, 1)"
   },
   View_30_558: {
-    width: wp("40%"),
-    top: hp("19%"),
+    width: ("40%"),
+    top: ("19%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("481%"),
+    left: ("481%"),
     justifyContent: "flex-start"
   },
   Text_30_558: {
@@ -361,12 +357,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_559: {
-    width: wp("55%"),
-    top: hp("32%"),
+    width: ("55%"),
+    top: ("32%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("482%"),
+    left: ("482%"),
     justifyContent: "flex-start"
   },
   Text_30_559: {
@@ -379,12 +375,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_560: {
-    width: wp("55%"),
-    top: hp("43%"),
+    width: ("55%"),
+    top: ("43%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("482%"),
+    left: ("482%"),
     justifyContent: "flex-start"
   },
   Text_30_560: {
@@ -397,12 +393,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_561: {
-    width: wp("55%"),
-    top: hp("56%"),
+    width: ("55%"),
+    top: ("56%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("481%"),
+    left: ("481%"),
     justifyContent: "flex-start"
   },
   Text_30_561: {
@@ -415,12 +411,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_562: {
-    width: wp("55%"),
-    top: hp("69%"),
+    width: ("55%"),
+    top: ("69%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("484%"),
+    left: ("484%"),
     justifyContent: "flex-start"
   },
   Text_30_562: {
@@ -433,12 +429,12 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_563: {
-    width: wp("38%"),
-    top: hp("106%"),
+    width: ("38%"),
+    top: ("106%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("508%"),
+    left: ("508%"),
     justifyContent: "flex-start"
   },
   Text_30_563: {
@@ -451,42 +447,42 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_564: {
-    width: wp("34%"),
-    height: hp("21%"),
-    top: hp("18%"),
+    width: ("34%"),
+    height: ("21%"),
+    top: ("18%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("6%"),
+    left: ("6%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   View_30_565: {
-    width: wp("2%"),
-    height: hp("1%"),
-    top: hp("57%"),
+    width: ("2%"),
+    height: ("1%"),
+    top: ("57%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("35%"),
+    left: ("35%"),
     backgroundColor: "rgba(255, 255, 255, 1)"
   },
   View_30_566: {
-    width: wp("42%"),
-    height: hp("7%"),
-    top: hp("102%"),
+    width: ("42%"),
+    height: ("7%"),
+    top: ("102%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("5%"),
+    left: ("5%"),
     backgroundColor: "rgba(255, 255, 255, 1)"
   },
   View_30_567: {
-    width: wp("20%"),
-    top: hp("104%"),
+    width: ("20%"),
+    top: ("104%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("17%"),
+    left: ("17%"),
     justifyContent: "flex-start"
   },
   Text_30_567: {
@@ -499,42 +495,42 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_568: {
-    width: wp("91%"),
-    height: hp("6%"),
-    top: hp("3%"),
+    width: ("91%"),
+    height: ("6%"),
+    top: ("3%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("5%"),
+    left: ("5%"),
     backgroundColor: "rgba(252, 255, 246, 1)"
   },
   View_30_569: {
-    width: wp("8%"),
-    height: hp("3%"),
-    top: hp("4%"),
+    width: ("8%"),
+    height: ("3%"),
+    top: ("4%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("6%"),
+    left: ("6%"),
     backgroundColor: "rgba(0, 0, 0, 0)"
   },
   ImageBackground_I30_569_30_64: {
     flexGrow: 1,
-    width: wp("6%"),
-    height: hp("3%"),
+    width: ("6%"),
+    height: ("3%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("1%"),
-    top: hp("0%")
+    left: ("1%"),
+    top: ("0%")
   },
   View_30_570: {
-    width: wp("52%"),
-    top: hp("4%"),
+    width: ("52%"),
+    top: ("4%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("18%"),
+    left: ("18%"),
     justifyContent: "flex-start"
   },
   Text_30_570: {
@@ -547,127 +543,127 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   View_30_571: {
-    width: wp("34%"),
-    height: hp("21%"),
-    top: hp("18%"),
+    width: ("34%"),
+    height: ("21%"),
+    top: ("18%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("59%"),
+    left: ("59%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   View_30_572: {
-    width: wp("34%"),
-    height: hp("20%"),
-    top: hp("46%"),
+    width: ("34%"),
+    height: ("20%"),
+    top: ("46%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("6%"),
+    left: ("6%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   View_30_573: {
-    width: wp("34%"),
-    height: hp("20%"),
-    top: hp("46%"),
+    width: ("34%"),
+    height: ("20%"),
+    top: ("46%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("59%"),
+    left: ("59%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   View_30_574: {
-    width: wp("34%"),
-    height: hp("20%"),
-    top: hp("77%"),
+    width: ("34%"),
+    height: ("20%"),
+    top: ("77%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("6%"),
+    left: ("6%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   View_30_575: {
-    width: wp("34%"),
-    height: hp("20%"),
-    top: hp("77%"),
+    width: ("34%"),
+    height: ("20%"),
+    top: ("77%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("59%"),
+    left: ("59%"),
     backgroundColor: "rgba(209, 220, 189, 1)"
   },
   ImageBackground_30_576: {
-    width: wp("30%"),
-    height: hp("18%"),
-    top: hp("19%"),
+    width: ("30%"),
+    height: ("18%"),
+    top: ("19%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("9%")
+    left: ("9%")
   },
   ImageBackground_30_577: {
-    width: wp("30%"),
-    height: hp("17%"),
-    top: hp("78%"),
+    width: ("30%"),
+    height: ("17%"),
+    top: ("78%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("9%")
+    left: ("9%")
   },
   ImageBackground_30_578: {
-    width: wp("29%"),
-    height: hp("18%"),
-    top: hp("19%"),
+    width: ("29%"),
+    height: ("18%"),
+    top: ("19%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("62%")
+    left: ("62%")
   },
   ImageBackground_30_579: {
-    width: wp("30%"),
-    height: hp("18%"),
-    top: hp("47%"),
+    width: ("30%"),
+    height: ("18%"),
+    top: ("47%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("9%")
+    left: ("9%")
   },
   ImageBackground_30_580: {
-    width: wp("29%"),
-    height: hp("18%"),
-    top: hp("47%"),
+    width: ("29%"),
+    height: ("18%"),
+    top: ("47%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("62%")
+    left: ("62%")
   },
   ImageBackground_30_581: {
-    width: wp("30%"),
-    height: hp("17%"),
-    top: hp("78%"),
+    width: ("30%"),
+    height: ("17%"),
+    top: ("78%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("62%")
+    left: ("62%")
   },
   View_30_582: {
-    width: wp("27%"),
-    minWidth: wp("27%"),
-    height: hp("6%"),
-    minHeight: hp("6%"),
+    width: ("27%"),
+    minWidth: ("27%"),
+    height: ("6%"),
+    minHeight: ("6%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("68%"),
-    top: hp("11%")
+    left: ("68%"),
+    top: ("11%")
   },
   View_30_583: {
-    width: wp("20%"),
-    top: hp("0%"),
+    width: ("20%"),
+    top: ("0%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("0%"),
+    left: ("0%"),
     justifyContent: "flex-start"
   },
   Text_30_583: {
@@ -680,46 +676,46 @@ const styles = StyleSheet.create({
     textTransform: "none"
   },
   ImageBackground_30_584: {
-    width: wp("7%"),
-    height: hp("0%"),
-    top: hp("2%"),
+    width: ("7%"),
+    height: ("0%"),
+    top: ("2%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("20%")
+    left: ("20%")
   },
   ImageBackground_30_585: {
-    width: wp("12%"),
-    minWidth: wp("12%"),
-    height: hp("7%"),
-    minHeight: hp("7%"),
+    width: ("12%"),
+    minWidth: ("12%"),
+    height: ("7%"),
+    minHeight: ("7%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("9%"),
-    top: hp("58%")
+    left: ("9%"),
+    top: ("58%")
   },
   ImageBackground_30_586: {
-    width: wp("13%"),
-    minWidth: wp("13%"),
-    height: hp("7%"),
-    minHeight: hp("7%"),
+    width: ("13%"),
+    minWidth: ("13%"),
+    height: ("7%"),
+    minHeight: ("7%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("6%"),
-    top: hp("59%")
+    left: ("6%"),
+    top: ("59%")
   },
   View_30_587: {
-    width: wp("2%"),
-    minWidth: wp("2%"),
-    height: hp("1%"),
-    minHeight: hp("1%"),
+    width: ("2%"),
+    minWidth: ("2%"),
+    height: ("1%"),
+    minHeight: ("1%"),
     marginLeft: 0,
     marginTop: 0,
     position: "absolute",
-    left: wp("36%"),
-    top: hp("47%"),
+    left: ("36%"),
+    top: ("47%"),
     backgroundColor: "rgba(255, 255, 255, 1)"
   }
 })
